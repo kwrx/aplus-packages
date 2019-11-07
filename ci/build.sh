3bc9d6eda844b47e6dbb4158c22bae45c7e2929b
Antonino Natale <antonio.natale97@hotmail.com>
2019-11-07 15:58:20 +0100

    Update Travis CI

--- ci/build.sh
+++ ci/build.sh
@@ -3,6 +3,6 @@
 for d in $(find $1 -maxdepth 1 -mindepth 1 -type d); do
     pushd $d
         echo "Packing $d..."
-        tar cJf ../../$(basename $d).tar.xz *
+        tar cJf ../../$(basename $d).tar.xz .
     popd
 done
