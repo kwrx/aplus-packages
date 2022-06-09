c05b3c26cc3aa58fde24cc733e71e22617093fe2
Antonino Natale <antonio.natale97@hotmail.com>
2022-06-09 02:17:04 +0200

    Update workflows

---
+++ ci/prepare.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "### Packing generic packages"
+
+packages="generic"
+
+for p in $packages; do
+    for d in $(find $p -maxdepth 1 -mindepth 1 -type d); do
+        pushd $d
+            echo "Packing $d..."
+            tar cJf ../../$(basename $d).tar.xz * .pkg
+        popd
+    done
+done
+
+
+echo "### Install dependencies"
+
+python3 -m pip install --user --upgrade pip
+python3 -m pip install --user --upgrade setuptools
+python3 -m pip install --user --upgrade wheel
+python3 -m pip install --user --upgrade twine
+python3 -m pip install -r requirements.txt
+
+
+
+echo "### Install toolchain"
+
+mkdir -p sdk
+
+pushd sdk
+wget https://github.com/kwrx/aplus-toolchain/releases/latest/download/x86_64-aplus-toolchain.tar.xz
+tar xJf x86_64-aplus-toolchain.tar.xz
+popd
+
+export PATH=$(pwd)/sdk/bin:$PATH
+
+
+echo "### Run build-system"
+
+python3 ci/build.py --clean
+
